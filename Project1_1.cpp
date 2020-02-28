40651168401d777e837b62aca6902f6610d74626
Inch-Z <1666161794@qq.com>
2020-02-29 00:31:14 +0800

    1

---
+++ Project1_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+int max(int a, int b, int c) {
+    return max(a, b) > c ? max(a, b) : c;
+}
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << max(a, b, c);
+}
