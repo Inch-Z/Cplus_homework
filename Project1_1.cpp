b9fb576d32c35f105b4489a210601aa486f60bd9
Inch-Z <1666161794@qq.com>
2020-02-29 13:36:31 +0800

    1

--- Project1_1.cpp
+++ Project1_1.cpp
@@ -10,4 +10,5 @@ int main() {
     int a, b, c;
     cin >> a >> b >> c;
     cout << max(a, b, c);
+    return 0;
 }
---
+++ Project1_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	for (int i = 0; i < 4; ++i) {
+		int number = 4 - i;
+		cout << setw(number) << "*";
+		for (int j = 0; j < i * 2; ++j) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < 3; ++i)	{
+		int	number = i + 2;
+		cout << setw(number) << "*";
+		for (int j = 0; j <4 - i * 2; ++j)	{
+			cout << "*";
+		}
+		cout << endl;
+		
+	}
+    return 0;	
+}
