7341fda32c5fc098bfc3d688fb59eefb2b61b426
Inch-Z <1666161794@qq.com>
2020-02-29 13:45:41 +0800

    1

--- Project1_2.cpp
+++ Project1_2.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int main() {
-	for (int i = 0; i < 4; ++i) {
+int main()
+{
+	for (int i = 0; i < 4; ++i)
+	{
 		int number = 4 - i;
 		cout << setw(number) << "*";
-		for (int j = 0; j < i * 2; ++j) {
+		for (int j = 0; j < i * 2; ++j)
+		{
 			cout << "*";
 		}
 		cout << endl;
 	}
-	for (int i = 0; i < 3; ++i)	{
-		int	number = i + 2;
+	for (int i = 0; i < 3; ++i)
+	{
+		int number = i + 2;
 		cout << setw(number) << "*";
-		for (int j = 0; j <4 - i * 2; ++j)	{
+		for (int j = 0; j < 4 - i * 2; ++j)
+		{
 			cout << "*";
 		}
 		cout << endl;
-		
 	}
-    return 0;	
+	return 0;
 }
